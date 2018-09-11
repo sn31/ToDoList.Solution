9e2ec4bc46ec8ebb92d758131525142d063144db
SkyeN <skye@dames.es>
2018-09-10 17:00:15 -0700

    connect MVC components

--- ToDoList/Controller/ItemsController.cs
+++ ToDoList/Controller/ItemsController.cs
@@ -1,28 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
     public class ItemsController : Controller
     {
-        [HttpPost("/items")]
+
+        [HttpGet("/items")]
         public ActionResult Index()
         {
-           List<Item> allItems = new List<Item> { };
+            List<Item> allItems = new List<Item> {};
             return View(allItems);
         }
+
+
         [HttpGet("/items/new")]
         public ActionResult CreateForm()
         {
             return View();
         }
+
         [HttpPost("/items")]
         public ActionResult Create()
         {
             Item newItem = new Item(Request.Form["new-item"]);
-            List<Item> allItems = new List<Item>{};
-            return View("Index",allItems);
+            List<Item> allItems = new List<Item> {};
+            return View("Index", allItems);
         }
+
     }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,75 +5,76 @@ using System.Linq;
 
 namespace ToDoList.Models
 {
-    public class Item
+  public class Item
+  {
+    private string _description;
+    private static List<Item> _instances = new List<Item> {};
+
+    public Item (string description)
     {
-        private string _description;
-        private static List<Item> _instances = new List<Item> { };
-        public Item(string description)
-        {
-            _description = description;
-        }
-        public string GetDescription()
-        {
-            return _description;
-        }
-        public void SetDescription(string newDescription)
-        {
-            _description = newDescription;
-        }
-        public static List<Item> GetAll()
-        {
-            return _instances;
-        }
-        public void Save()
-        {
-            _instances.Add(this);
-        }
-        public static void ClearAll()
-        {
-            _instances.Clear();
-        }
+      _description = description;
     }
+    public string GetDescription()
+    {
+      return _description;
+    }
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+    public void Save()
+    {
+      _instances.Add(this);
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
 }
 
-class Program
-{
-    public static void Main()
-    {
+// class Program
+// {
+//     public static void Main()
+//     {
 
-        Console.WriteLine("Welcome to the To do");
-        Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View/Exit)");
-        string userAction = Console.ReadLine().ToLower();
-        while (userAction == "add")
-        {
-            Console.WriteLine("Please enter the description for the new item.");
-            string description = Console.ReadLine();
-            Item myItem = new Item(description);
-            myItem.Save();
-            Console.WriteLine("'{0}' has been added to your list. Would you like to add an item to your list or view your list? (Add/View/Exit)", myItem.GetDescription());
-            userAction = Console.ReadLine().ToLower();
-        }
-        if (userAction == "view")
-        {
-            List<Item> instances = Item.GetAll();
-            if (!instances.Any())
-            {
-                Console.WriteLine("You have no item to view! Would you like to add an item? (Add/Exit)");
-                userAction = Console.ReadLine().ToLower();
-            }
-            else
-            {
-                Console.WriteLine("Your To Do List: ");
-                foreach (Item thisItem in instances)
-                {
-                    Console.WriteLine(thisItem.GetDescription());
-                }
-            }
+//         Console.WriteLine("Welcome to the To Do List");
+//         Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View/Exit)");
+//         string userAction = Console.ReadLine().ToLower();
+//         while (userAction == "add")
+//         {
+//             Console.WriteLine("Please enter the description for the new item.");
+//             string description = Console.ReadLine();
+//             Item myItem = new Item(description);
+//             myItem.Save();
+//             Console.WriteLine("'{0}' has been added to your list. Would you like to add an item to your list or view your list? (Add/View/Exit)", myItem.GetDescription());
+//             userAction = Console.ReadLine().ToLower();
+//         }
+//         if (userAction == "view")
+//         {
+//             List<Item> instances = Item.GetAll();
+//             if (!instances.Any())
+//             {
+//                 Console.WriteLine("You have no item to view! Would you like to add an item? (Add/Exit)");
+//                 userAction = Console.ReadLine().ToLower();
+//             }
+//             else
+//             {
+//                 Console.WriteLine("Your To Do List: ");
+//                 foreach (Item thisItem in instances)
+//                 {
+//                     Console.WriteLine(thisItem.GetDescription());
+//                 }
+//             }
 
-        }
-        else if (userAction == "exit")
-        {
-            Console.WriteLine("Goodbye!");
-        }
-    }
-}
+//         }
+//         else if (userAction == "exit")
+//         {
+//             Console.WriteLine("Goodbye!");
+//         }
+//     }
+// }
