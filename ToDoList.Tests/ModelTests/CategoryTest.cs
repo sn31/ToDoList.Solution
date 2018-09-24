cdf8bdeb04a4b8d6e73b57f8f96ae0b23d20c277
SkyeN <skye@dames.es>
2018-09-24 09:02:37 -0700

    add previous tests

---
+++ ToDoList.Tests/ModelTests/CategoryTest.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+
+    [TestClass]
+    public class CategoryTest : IDisposable
+    {
+        public CategoryTest()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=todo_test;";
+        }
+        public void Dispose()
+        {
+            Category.ClearAll();
+            Category.ClearAll();
+        }
+
+        [TestMethod]
+        public void GetAll_DatabaseEmptyAtFirst_0()
+        {
+            //Arrange, Act
+            int result = Category.GetAll().Count;
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Equals_TrueForSameDescription_Category()
+        {
+            //Arrange, Act
+            Category firstCategory = new Category("School work");
+            Category secondCategory = new Category("School work");
+
+            //Assert
+            Assert.AreEqual(firstCategory, secondCategory);
+        }
+
+        [TestMethod]
+        public void Save_CategorySavesToDatabase_CategoryList()
+        {
+            //Arrange
+            Category testCategory = new Category("Mow the lawn");
+            testCategory.Save();
+
+            //Act
+            List<Category> result = Category.GetAll();
+            List<Category> testList = new List<Category> { testCategory };
+
+            //Assert
+            CollectionAssert.AreEqual(testList, result);
+        }
+
+        [TestMethod]
+        public void Save_AssignsIdToObject_id()
+        {
+            //Arrange
+            Category testCategory = new Category("Mow the lawn");
+            testCategory.Save();
+
+            //Act
+            Category savedCategory = Category.GetAll() [0];
+
+            int result = savedCategory.GetId();
+            int testId = testCategory.GetId();
+
+            //Assert
+            Assert.AreEqual(testId, result);
+        }
+        [TestMethod]
+        public void Find_FindsCategoryInDatabase_Category() //Test is failing for no reason?
+        {
+            //Arrange
+            Category testCategory = new Category("Mow the lawn");
+            testCategory.Save();
+
+            //Act
+            Category result = Category.Find(testCategory.GetId());
+
+            //Assert
+            Assert.AreEqual(testCategory, result);
+        }
+    }
+}
